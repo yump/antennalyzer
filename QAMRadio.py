535300ffed862f7418d8aa3d18db8465130b9d22
Russell Haley <rhaley@tamu.edu>
2013-04-08 11:58:35 -0500

    We GPLv3 now.

--- QAMRadio.py
+++ QAMRadio.py
@@ -1,20 +1,20 @@
 #   Copyright 2012,2013 Russell Haley
 #   (Please add yourself if you make changes)
 #
-#   This file is part of doa-backend.
+#   This file is part of swr-recorder.
 #
-#   doa-backend is free software: you can redistribute it and/or modify
+#   swr-recorder is free software: you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
 #   the Free Software Foundation, either version 3 of the License, or
 #   (at your option) any later version.
 #
-#   doa-backend is distributed in the hope that it will be useful,
+#   swr-recorder is distributed in the hope that it will be useful,
 #   but WITHOUT ANY WARRANTY; without even the implied warranty of
 #   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #   GNU General Public License for more details.
 #
 #   You should have received a copy of the GNU General Public License
-#   along with doa-backend.  If not, see <http://www.gnu.org/licenses/>.
+#   along with swr-recorder.  If not, see <http://www.gnu.org/licenses/>.
 
 class QAMRadio:
 	"""Superclass for QAM radios."""
