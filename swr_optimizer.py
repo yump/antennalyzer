818350ef0ebcecbf1ac29af9b0255d4aeb61cad4
Russell Haley <rhaley@tamu.edu>
2013-04-09 12:31:24 -0500

    We GPL3 now.

--- swr_optimizer.py
+++ swr_optimizer.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
+
+#   Copyright 2012,2013 Russell Haley
+#   (Please add yourself if you make changes)
+#
+#   This file is part of swr_optimizer.
+#
+#   swr_optimizer is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   swr_optimizer is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with swr_optimizer.  If not, see <http://www.gnu.org/licenses/>.
+
 """
 swr_optimizer : Find an optimal subset of a set of antennas, based on VSWR.
 
