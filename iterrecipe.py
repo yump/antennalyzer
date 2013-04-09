72d7d77a14eaba71673c309946fb8fd0df1a04e3
Russell Haley <rhaley@tamu.edu>
2013-04-09 12:29:48 -0500

    Forgot to put this file in git.  Oops.  From Python itertools documentation.

---
+++ iterrecipe.py
@@ -0,0 +1,6 @@
+from itertools import zip_longest
+
+def grouper(n, iterable, fillvalue=None):
+    "grouper(3, 'ABCDEFG', 'x') --> ABC DEF Gxx"
+    args = [iter(iterable)] * n
+    return zip_longest(*args, fillvalue=fillvalue)
