517f1cc4334efd851c464e8f6c336dc202612d74
Russell Haley <rhaley@tamu.edu>
2013-04-11 22:22:27 -0500

    Convert tabs to spaces.

--- FieldFox.py
+++ FieldFox.py
@@ -22,72 +22,72 @@ import numpy as np
 
 class FieldFox(QAMRadio):
 
-	"""A class to communicate with the Agilent FieldFox network analyzer."""
+    """A class to communicate with the Agilent FieldFox network analyzer."""
 
-	def __init__(self, host, port=5025, numpoints=1001, lo=2.35e9, hi=2.55e9):
-		self.numpoints = numpoints
-		self.conn = telnetlib.Telnet(host, port)
-		#self.scpi("*RST")                 # apparently this breaks calbration
-		self.barrier()
-		self.scpi("CALC:PAR1:DEF S11")     # measure S11
-		self.scpi("CALC:SEL:FORM SWR")     # SWR
-		self.scpi("INIT:CONT 1")           # continuous sweep for interactivity
-		self.scpi("SENS:SWE:POIN {}".format(numpoints)) # number of points
-		self.scpi("SENS:FREQ:STAR {}".format(lo))
-		self.scpi("SENS:FREQ:STOP {}".format(hi))
-		self.scpi(":BWID 10e3")             # 10kHz IF bandwidth
-		self.sync()
+    def __init__(self, host, port=5025, numpoints=1001, lo=2.35e9, hi=2.55e9):
+        self.numpoints = numpoints
+        self.conn = telnetlib.Telnet(host, port)
+        #self.scpi("*RST")                 # apparently this breaks calbration
+        self.barrier()
+        self.scpi("CALC:PAR1:DEF S11")     # measure S11
+        self.scpi("CALC:SEL:FORM SWR")     # SWR
+        self.scpi("INIT:CONT 1")           # continuous sweep for interactivity
+        self.scpi("SENS:SWE:POIN {}".format(numpoints)) # number of points
+        self.scpi("SENS:FREQ:STAR {}".format(lo))
+        self.scpi("SENS:FREQ:STOP {}".format(hi))
+        self.scpi(":BWID 10e3")             # 10kHz IF bandwidth
+        self.sync()
 
-	def __del__(self):
-		self.conn.close()
+    def __del__(self):
+        self.conn.close()
 
-	def sample(self, avg=3):
-		"""
-		Collect the SWR for a range of values.  Result is a 1 dimensional
-		numpy array.
+    def sample(self, avg=3):
+        """
+        Collect the SWR for a range of values.  Result is a 1 dimensional
+        numpy array.
 
-		Parameters
-		----------
-		avg : int
-			Number of sweeps to average.
-		"""
-		accum = np.zeros(self.numpoints)
-		self.scpi("INIT:CONT 0")           # single sweep mode
-		self.barrier()
-		for ix in range(avg):
-			self.scpi("INIT")
-			self.barrier()
-			self.scpi("CALC:DATA:FDAT?")    # SWR sample
-			answer =  self.conn.read_until(b'\n').decode('ascii')
-			parsed = answer.strip().split(",")
-			accum += [float(sym) for sym in parsed]
-		self.scpi("INIT:CONT 1")           # back to continuous sweep
-		return accum / avg
+        Parameters
+        ----------
+        avg : int
+            Number of sweeps to average.
+        """
+        accum = np.zeros(self.numpoints)
+        self.scpi("INIT:CONT 0")           # single sweep mode
+        self.barrier()
+        for ix in range(avg):
+            self.scpi("INIT")
+            self.barrier()
+            self.scpi("CALC:DATA:FDAT?")    # SWR sample
+            answer =  self.conn.read_until(b'\n').decode('ascii')
+            parsed = answer.strip().split(",")
+            accum += [float(sym) for sym in parsed]
+        self.scpi("INIT:CONT 1")           # back to continuous sweep
+        return accum / avg
 
-	def sync(self):        
-		"""
-		Wait for completion of pending commands.
-		"""
-		self.scpi("*OPC?")
-		self.conn.read_until(b'\n')
+    def sync(self):        
+        """
+        Wait for completion of pending commands.
+        """
+        self.scpi("*OPC?")
+        self.conn.read_until(b'\n')
 
-	def barrier(self):
-		"""
-		Force previous commands to finish before subsequent commands..
-		"""
-		self.scpi("*WAI")
+    def barrier(self):
+        """
+        Force previous commands to finish before subsequent commands..
+        """
+        self.scpi("*WAI")
 
-	def scpi(self, command):
-		"""
-		Send a string to the SCPI instrument.
-		Handles encoding and termination.
-		"""
-		self.conn.write(command.encode('ascii') + b'\n')
+    def scpi(self, command):
+        """
+        Send a string to the SCPI instrument.
+        Handles encoding and termination.
+        """
+        self.conn.write(command.encode('ascii') + b'\n')
 
-	def command(self, command):
-		"""
-		Send an SCPI command and return the result. For human interactive use.
-		"""
-		self.scpi(command)
-		return conn.read_until(b'\n').decode('ascii')
+    def command(self, command):
+        """
+        Send an SCPI command and return the result. For human interactive use.
+        """
+        self.scpi(command)
+        return conn.read_until(b'\n').decode('ascii')
 
--- swr_recorder.py
+++ swr_recorder.py
@@ -32,22 +32,22 @@ file = 'antenna_swr'
 datadict = {}
 
 while True:
-	try:
-		ant = int(input("Antenna number?"))
-		pol = input("Antenna polarization (x/y)")
-		if pol == "x" or pol == "y":
-			pass
-		else:
-			raise ValueError("Bad polarization")
-			log.warn("User entered polarization other than 'x' or 'y'")
-	except ValueError:
-		print("Bad antenna number or polarization.")
-	except KeyboardInterrupt:
-		print("Exiting...")
-		log.debug("Exiting due to keyboard interrupt")
-		exit(0)
-	else:
-		log.info("Sampling antenna {} polariztion {}".format(ant,pol))
-		swrsamples = meter.sample(avg=5)
-		datadict.update({"{}_{}".format(ant,pol) : swrsamples})
-		np.savez(file,**datadict)
+    try:
+        ant = int(input("Antenna number?"))
+        pol = input("Antenna polarization (x/y)")
+        if pol == "x" or pol == "y":
+            pass
+        else:
+            raise ValueError("Bad polarization")
+            log.warn("User entered polarization other than 'x' or 'y'")
+    except ValueError:
+        print("Bad antenna number or polarization.")
+    except KeyboardInterrupt:
+        print("Exiting...")
+        log.debug("Exiting due to keyboard interrupt")
+        exit(0)
+    else:
+        log.info("Sampling antenna {} polariztion {}".format(ant,pol))
+        swrsamples = meter.sample(avg=5)
+        datadict.update({"{}_{}".format(ant,pol) : swrsamples})
+        np.savez(file,**datadict)
